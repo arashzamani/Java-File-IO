10a9b22bb19c03a27ead375c5938692e13f05427
Arash Zamani Farahani <arash.zamani@bihe.org>
2015-11-14 20:01:19 +0330

    Version 0.1.2
    Signed-off-by: Arash Zamani Farahani <arash.zamani@bihe.org>

--- FileIO/src/org/bihe/main/Main.java
+++ FileIO/src/org/bihe/main/Main.java
@@ -1,18 +1,39 @@
 package org.bihe.main;
 
-import java.awt.image.TileObserver;
 import java.util.HashMap;
+
+import org.bihe.util.FileIO;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		HashMap<String, Integer> test = new HashMap<String, Integer>();
 		test.put("Arash", 5);
 		test.put("Zamani", 6);
 		test.put("Farahani", 8);
-		
-		//test the FileIO methods
-		
+
+		// test the FileIO methods
+		boolean result = FileIO.writeObjectToFile(test, "test.obj");
+		if (result)
+			System.out
+					.println("object has been written in the file successfully");
+		else
+			System.err
+					.println("a problem has been occured in writting object to file");
+
+		Thread.sleep(2000);
+
+		Object dataResult = FileIO.readObjectFromFile("test.obj");
+		if (dataResult == null) {
+			System.err
+					.println("a problem has been occured in reading object from file");
+		} else {
+			HashMap<String, Integer> loadedData = (HashMap<String, Integer>) dataResult;
+			for (String key : loadedData.keySet()) {
+				Integer value = loadedData.get(key);
+				System.out.println("key: "+key+" # value: "+value);
+			}
+		}
 
 	}
 
--- FileIO/src/org/bihe/util/FileIO.java
+++ FileIO/src/org/bihe/util/FileIO.java
@@ -8,24 +8,30 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 /**
- * This class load an object from input file and also write an object to the file. 
- * It includes some methods to do it in easy way and you can use anywhere
- * @author Arash Zamani Farahani 
- * @version 0.1.1
- * {@link http://docs.oracle.com/javase/7/docs/api/java/io/FileOutputStream.html}
- * Contact Me: arash.zamani@bihe.org - I'm Student at BIHE.
- * for more information about BIHE you can chcek www.bihe.org
+ * This class load an object from input file and also write an object to the
+ * file. It includes some methods to do it in easy way and you can use anywhere
+ * 
+ * @author Arash Zamani Farahani
+ * @version 0.1.2 {@link http
+ *          ://docs.oracle.com/javase/7/docs/api/java/io/FileOutputStream.html}
+ *          Contact Me: arash.zamani@bihe.org - I'm Student at BIHE. for more
+ *          information about BIHE you can chcek www.bihe.org
  *
  */
 public class FileIO {
 	/**
+	 * this method write an object to the file. the file have to be serializable. otherwise,
+	 * an exception will be happened.
+	 * @param obj the object will be write to the file
+	 * @param filePath the String of file path for writing in the file
+	 * @return true if the object has been written successfully and false if an exception throws while writing the object in the file.
+	 * @throws FileNotFoundException if the file was not found
 	 * 
-	 * @param obj
-	 * @param filePath
-	 * @return
+	 * 
 	 */
-	public static boolean writeObjectToFile(Object obj, String filePath){
-		//At the first step we define a boolean. It initialize with false value. 
+	public static boolean writeObjectToFile(Object obj, String filePath) {
+		// At the first step we define a boolean. It initialize with false
+		// value.
 		boolean result = false;
 		FileOutputStream fos = null;
 		try {
@@ -50,18 +56,19 @@ public class FileIO {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return result;
 	}
-	//==================================================================
-	//==================================================================
-	//==================================================================
+
+	// ==================================================================
+	// ==================================================================
+	// ==================================================================
 	/**
-	 * 
-	 * @param filePath
-	 * @return
+	 * This method read an object from the file. 
+	 * @param filePath the String of file path for reading from the file
+	 * @return null if an exception throws reading object from file or an object from Object class if everything is going ok.
 	 */
-	public static Object readObjectFromFile(String filePath){
+	public static Object readObjectFromFile(String filePath) {
 		Object result = null;
 		FileInputStream fis = null;
 		try {
@@ -78,7 +85,7 @@ public class FileIO {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			try {
 				fis.close();
 			} catch (IOException e) {
@@ -87,5 +94,5 @@ public class FileIO {
 		}
 		return result;
 	}
-	
+
 }
